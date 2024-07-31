619b49ea1fa0b6df4ffe092dc21c8dbc3b498f86
Alex Handley <smithvillescholarsah@gmail.com>
2024-07-30 19:58:17 -0500

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require("../../data/db-config.js");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,20 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const item = await db('schemes')
+    .where('scheme_id', req.params.scheme_id)
+    .first();
 
+    if (!item) {
+      next({ status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found` });
+    } else {
+      next();
+    }
+  } catch(error) {
+    next(error);
+  }
 }
 
 /*
